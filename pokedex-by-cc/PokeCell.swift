6f36ae8a3e15ac707d27015cdf2538a841f64ed7
Tyler Asmus <tasmus@MacBook-Pro-2.local>
2016-06-22 16:59:54 -0400

    Cells and pokemon added. Lists in grid. Starting to parse CSV files

---
+++ pokedex-by-cc/PokeCell.swift
@@ -0,0 +1,31 @@
+//
+//  PokeCell.swift
+//  pokedex-by-cc
+//
+//  Created by Tyler Asmus on 6/22/16.
+//  Copyright © 2016 Tyler Asmus. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+}
---
+++ pokedex-by-cc/Pokemon.swift
@@ -0,0 +1,28 @@
+//
+//  Pokemon.swift
+//  pokedex-by-cc
+//
+//  Created by Tyler Asmus on 6/22/16.
+//  Copyright © 2016 Tyler Asmus. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+}
